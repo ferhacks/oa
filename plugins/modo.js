23e2282e2e8070ba9bf2fd007738080240f61fae
ferhacks <fernandovillaverde40@gmail.com>
2021-03-27 23:34:58 +0000

    a

--- plugins/modo.js
+++ plugins/modo.js
@@ -1,8 +1,8 @@
 let { Presence, GroupSettingChange } = require('@adiwajshing/baileys')
 let handler  = async (m, { conn, args, usedPrefix, command }) => {
 	let isClose = { // Switch Case Like :v
-		'open': unavailable,
-		'close': available,
+		'1': unavailable,
+		'0': available,
 	}[(args[0] || '')]
 	if (isClose === undefined)
 		throw `
@@ -10,7 +10,7 @@ lo isiste mal
 `.trim()
 await conn.updatePresence(m.chat, isClose)
 }
-handler.help = ['group *open / close*']
+handler.help = ['mood *1 / 0*']
 handler.tags = ['group']
 handler.command = /^(mood)$/i
 handler.owner = false
