906c992ee0b4f5996b9265bd7183b79be360184d
ferhacks <fernandovillaverde40@gmail.com>
2021-03-27 23:35:51 +0000

    a

--- plugins/modo.js
+++ plugins/modo.js
@@ -13,13 +13,13 @@ await conn.updatePresence(m.chat, isClose)
 handler.help = ['mood *1 / 0*']
 handler.tags = ['group']
 handler.command = /^(mood)$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
-handler.admin = true
-handler.botAdmin = true
+handler.admin = false
+handler.botAdmin = false
 handler.fail = null
 handler.exp = 0
 module.exports = handler
